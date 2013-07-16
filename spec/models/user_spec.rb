1e2e0053770109961d014fa059ff3b56f812d26d
Bob <loveccb@hotmail.com>
2013-07-16 16:16:29 -0500

    finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,26 +18,34 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   end
 
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }
-  
-
-  subject { @user }
+  subject { @user }  
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
-  it { should be_valid }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+  
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
   
 
 #password
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,8 +10,8 @@ def full_title(page_title)
 end
 
 def valid_signin(user) 
-	fill_in "Email", 	with: user.email
-	fill_in "Password" 	with: user.password
+	fill_in "Email", 		with: user.email
+	fill_in "Password", 	with: user.password
 	click_button "Sign in"
 end
 
@@ -19,4 +19,13 @@ RSpec::Matchers.define	:have_error_message do |messages|
 	match do |page| 
 		page.should have_selector('div.alert.alert-error', text: message)
 	end
+end
+
+def sign_in(user) 
+	visit signin_path
+	fill_in "Email", 	with: user.email
+	fill_in "Password",	with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
 end
